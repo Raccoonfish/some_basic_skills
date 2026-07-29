41f7f42cb124611840e36fd21edc37e04376b06f
raccoon <elinaisemgulova@gmail.com>
2026-07-29 11:10:15 +0500

    some comments

--- DICOM_bash/AddDICOMnodes.sh
+++ DICOM_bash/AddDICOMnodes.sh
@@ -6,6 +6,9 @@ targ_dir="/opt/weasis/lib/app/resources"
 sudo cp dicomCallingNodes.xml ${targ_dir}/dicomCallingNodes.xml
 sudo cp dicomNodes.xml ${targ_dir}/dicomNodes.xml
 
+
+#I added this part out of pure interest to see if I could simulate closing and opening a program from the GUI using the terminal
+#better not to use it 
 pkill -f weasis
 sleep 3
 /opt/weasis/bin/Weasis &> /dev/null &
